c28362d80dc72e9f1e6d3959a033344e1bb45a7b
shenjack <3695888@qq.com>
2024-06-18 21:24:58 +0800

    Create make-zed.ps1
    require banizip and b3sum

---
+++ make-zed.ps1
@@ -0,0 +1,62 @@
+$current_pos = Get-Location
+Write-Host "当前位置：$current_pos"
+
+Set-Location V:\githubs\zed
+Write-Host "更新 Zed 仓库"
+git pull
+Write-Host "更新 Zed 仓库完成"
+
+# 准备构建信息
+$date = Get-Date -Format "yyyy-MM-dd-HH_mm_ss"
+Write-Host "更新时间：$date"
+$commit = git log -1 --pretty=format:"%h"
+Write-Host "最新提交：$commit"
+$zip_name = "zed-$date-$commit-ziped.zip"
+$zip_namex = "zed-$date-$commit-ziped.zipx"
+Write-Host "ZIP 名称：$zip_name"
+Write-Host "ZIPX 名称：$zip_namex"
+# 上面这堆信息构建完会再输出一遍
+
+# build!
+Write-Host "开始构建"
+$start_time = Get-Date
+cargo build --release
+Write-Host "构建完成, 耗时：$((Get-Date) - $start_time)"
+
+# 把最新构建 copy 到 D:\path-scripts
+Copy-Item -Path ".\target\release\Zed.exe" -Destination "D:\path-scripts\Zed.exe" -Force
+
+# 到 D:\path-scripts 目录
+Set-Location "D:\path-scripts"
+if (Test-Path $zip_name) {
+    Write-Host "删除旧 ZIP"
+    Remove-Item -Path $zip_name -Force
+}
+Write-Host "开始打包"
+
+Write-Host "打包信息:"
+Write-Host "  - ZIP 名称：$zip_name"
+Write-Host "  - ZIPX 名称：$zip_namex"
+Write-Host "  - commit id: $commit"
+Write-Host "  - 构建时间：$date"
+Write-Host "  - 构建耗时：$((Get-Date) - $start_time)"
+
+# 创建一个 zed-zip 文件夹
+if (-not (Test-Path ".\zed-zip")) {
+    New-Item -ItemType Directory -Name "zed-zip" -Force
+}
+
+# 忽略输出
+$_ = bz.exe c -l:9 -y -fmt:zip -t:14 -cp:65001 .\zed-zip\$zip_name .\Zed.exe
+$_ = bz.exe c -l:9 -y -fmt:zipx -t:14 -cp:65001 .\zed-zip\$zip_namex .\Zed.exe
+$_ = bz.exe t .\zed-zip\$zip_name
+$_ = bz.exe t .\zed-zip\$zip_namex
+
+# 计算 hash
+Write-Host "blake3sum:"
+b3sum.exe .\zed-zip\$zip_name
+b3sum.exe .\zed-zip\$zip_namex
+Write-Host "ZIP 压缩完成"
+
+# 返回原位置
+Set-Location $current_pos
